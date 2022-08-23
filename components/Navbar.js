93e9fe0db4ab4fc4dfa2785d6843d0ff1bba0b84
Jeon316upzx <mamasbwoyjeon94@gmail.com>
2022-08-23 17:49:28 +0100

    rsvp finalized

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,13 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import Image from "next/image";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount, useDisconnect } from "wagmi";
 import Navmenu from "./Navmenu";
 
 export default function Navbar() {
+  const { data: account } = useAccount();
+  const { disconnect } = useDisconnect();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -18,8 +23,9 @@ export default function Navbar() {
         >
           <div className="w-full py-6 flex flex-wrap items-center justify-between border-b border-indigo-500 lg:border-none">
             <div className="flex items-center">
-              <Link href="/">
-                <a>web3rsvp</a>
+              <Image src={"/logos/dog.png"} width={30} height={30}/>
+              <Link href="/" >
+                <a className="font-bold text-lg ml-2">Event Gwigu </a>
               </Link>
             </div>
             <div className="ml-10 space-x-4 flex items-center">
@@ -28,6 +34,12 @@ export default function Navbar() {
                   Create Event
                 </a>
               </Link>
+
+              {account ? (
+                <Navmenu account={account} disconnect={() => disconnect()} />
+              ) : (
+                <ConnectButton />
+              )}
             </div>
           </div>
         </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,43 @@
 import Layout from "../components/Layout";
+
+//Rainbow
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
 import "../styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
+import client from "../utils/apollo-client";
+
+const infuraId = process.env.NEXT_PUBLIC_INFURA_ID;
+
+const { chains, provider } = configureChains(
+  [chain.rinkeby],
+  [infuraProvider({ infuraId }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "RSVP",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <ApolloProvider client={client}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ApolloProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
